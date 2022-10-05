104a68d02b400033b24c35f34bc5ca12337ee7c4
g2 <ke0bhogsg@gmail.com>
2022-10-05 08:26:46 -0500

    Added files

---
+++ phys.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+stty -echo # Disable keyboard output
+
+source options.cfg # Grab configs
+source functions
+
+startup
+
+while true; do
+    input       #read keyboard input
+
+    move        #move the character
+
+    movecalc    #update movement parameters
+
+    render      #render the player, stage, and platforms
+
+    info        #display the velocity and position information
+
+    tput cup 21 80
+    if [[ "$input" = "q" ]] || [[ $win -gt 0 ]]; then
+        stty echo
+        tput cup 22 0
+        textbox 10 39 "Exiting Game"
+        kill %1 &> /dev/null
+        exit 0
+    fi
+
+    sleep $fps
+done
